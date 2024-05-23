8bc49ede1b34ded41867afb791fbe376b4a917b1
Ye Yint Ko Ko <yeyintkoko.1788@gmail.com>
2024-05-23 13:10:45 +0700

    initial

---
+++ app/src/main/java/com/example/weatherapp/WeatherApplication.kt
@@ -0,0 +1,20 @@
+package com.example.weatherapp
+
+import android.app.Application
+import android.content.Context
+
+class WeatherApplication : Application() {
+    companion object {
+        val TAG = WeatherApplication::class.java
+        lateinit var instance : WeatherApplication
+
+        fun getContext(): Context {
+            return instance
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+}
